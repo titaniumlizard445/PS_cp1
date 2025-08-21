4228d5c4e8044c51e531a8629cc80b98e5c6c500
titaniumlizard445 <parker.swenson@ucas-edu.net>
2025-08-21 09:52:32 -0600

    variables notes

---
+++ Notes/Variables.py
@@ -0,0 +1,8 @@
+#PS 1st Variable Notes
+
+even_number = 2
+grade = 99
+animal = "Panda"
+print(grade)
+grade = 95
+print(grade)
---
+++ Starters/seasons.py
@@ -0,0 +1,8 @@
+# PS 1st Seasons
+lastname = input("Enter your last name here:")
+
+Favorite_Season = input("Enter your favorite season here:")
+
+print("Wow" , lastname , "san, I like" , Favorite_Season , "too!")
+
+ 
