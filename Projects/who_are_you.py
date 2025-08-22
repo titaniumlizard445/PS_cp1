c91f12a4eca79052314c5801721ec849fab4f27d
titaniumlizard445 <parker.swenson@ucas-edu.net>
2025-08-22 09:53:12 -0600

    Needs to be finished

---
+++ Projects/who_are_you.py
@@ -0,0 +1,22 @@
+#PS 1st Project
+running = True
+Users = []
+dictionary = {}
+while running:
+
+    current_user = input("Enter your name here:")
+    if current_user in Users:
+        print("You have already answered")
+
+    else:
+        Users.append(current_user)
+        age = input("Enter your age:")
+        fav_color = input("What is your favorite color:")
+        dictionary[current_user , "Age"] = age
+        dictionary[current_user , "Favorite color"] = fav_color
+        print(dictionary)
+    
+
+Users.clear()
+dictionary.clear()
+
---
+++ Starters/Starter.py
@@ -0,0 +1,11 @@
+# PS 1st Starter
+num1 = 5
+num2 = 7
+result = num1 + num2
+print(result)
+
+
+num3 = 3.5
+num4 = 2.0
+result2 = num3 / num4
+print(result2)
