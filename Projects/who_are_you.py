a4a23198eda2992d6fc60ebf5540c3e9f944647a
titaniumlizard445 <parker.swenson@ucas-edu.net>
2025-08-25 09:46:35 -0600

    Debugged and done

--- Projects/who_are_you.py
+++ Projects/who_are_you.py
@@ -1,4 +1,4 @@
-#PS 1st Project
+#PS 1st who_are_you
 running = True
 Users = []
 dictionary = {}
@@ -7,15 +7,19 @@ while running:
     current_user = input("Enter your name here: ")
     if current_user in Users:
         print("You have already answered")
-        print(dictionary)
+        print("You are",current_user, "and you are", dictionary[current_user,"Age"], "years old, and your favorite color is", dictionary[current_user,"Favorite color"]+".")
 
     else:
         Users.append(current_user)
-        age = input("Enter your age:")
+        
+        age = input("Enter your age: ")
         fav_color = input("What is your favorite color: ")
+        
         dictionary[current_user , "Age"] = age
         dictionary[current_user , "Favorite color"] = fav_color
+        
         print(dictionary)    
+    
     end = input("End the program?: ")
     if end == "yes":
         running = False
