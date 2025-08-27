c44b13b1064ce4fcef5878daf7c36a5269aa91a4
titaniumlizard445 <parker.swenson@ucas-edu.net>
2025-08-27 09:53:42 -0600

    VAR

---
+++ Notes/Variable_Types.py
@@ -0,0 +1,39 @@
+#PS 1 integers and floats notes
+
+age = 999999999999999999999
+year = 2015
+room = 200
+hour = 9
+motivation = -5
+
+
+pi = 3.14
+percent = .75
+damage = 9.75
+
+#Math
+
+print(5.0+2) #7
+print(5-2) #3
+print(5*2) #10
+print(5/2) #2.5
+print(5//2) #2
+print(5%2) #1
+print(5**2) #25
+
+print( 5+2 *9 + (5**5)%3)
+
+#Type casting
+
+print(int(6.98))
+print(float(6))
+
+#Using variables
+
+hours = 3
+minutes = 4
+print(hours * 60 + minutes)
+
+
+#To change a user input data type you have to surround the variable
+age = int(input("How old are you: " ))
