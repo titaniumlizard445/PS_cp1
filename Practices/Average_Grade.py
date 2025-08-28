40253d55e2f61e31282dd32b3c1e526c0bcd533e
titaniumlizard445 <parker.swenson@ucas-edu.net>
2025-08-28 09:45:56 -0600

    Message

--- Practices/Average_Grade.py
+++ Practices/Average_Grade.py
@@ -4,11 +4,9 @@ ClassGrades = []
 ClassNumber = int(input("how many classes do you have"))
 Average = 0
 for i in range(0, ClassNumber):
-    ClassGrades.append(input(f"what is your grade in class, {i}."))
-
+    ClassGrades.append(float(input(f"what is your grade in class, {i}.")))
 for i in ClassGrades:
     Average = Average + int(i)
-
 Average /= ClassNumber
 Average = round(Average, 2)
 print(Average)
