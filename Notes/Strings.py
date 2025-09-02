ad5dbe3a84c73ae0a244999220ac8543a0dcfb60
titaniumlizard445 <parker.swenson@ucas-edu.net>
2025-09-02 09:53:28 -0600

    push

---
+++ Notes/Strings.py
@@ -0,0 +1,32 @@
+#PS 1st Strings notes
+
+complaint = "Really hard thing to do"
+
+first_name = "Someone you want to know"
+month = "something you wish to have"
+book = "something you wish to read"
+food = "something you with to eat"
+
+print(month)
+
+length = len(first_name)
+print(length)
+
+
+print('the "Literal" best string.')
+#escape characters
+
+print("\tthe \"Literal\" best \n string.")
+
+#concatnate
+
+last_name = "something that you wish to not know"
+full_name = first_name + last_name
+
+print(full_name)
+
+#index
+
+print(last_name[0:8])
+print(last_name[2:-2])
+
