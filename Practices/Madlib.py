ffa45f6117d8a998c6b385b62c383a898ed50480
titaniumlizard445 <parker.swenson@ucas-edu.net>
2025-09-03 09:54:11 -0600

    push

---
+++ Practices/Madlib.py
@@ -0,0 +1,21 @@
+#PS 1st Madlib
+
+person_name = input("name a person: ")
+event = input("name a type of event: ")
+name_of_a_food = input("name a food: ")
+celebrity = input("name a celebrity: ")
+emotion = input("name an emotion: ")
+verb_with_ing = input("name a verb ending with ing: ")
+body_part = input("name a body part: ")
+number = input("name a large number: ")
+job_title = input("name a job title: ")
+adjective = input("name an adjective: ")
+
+
+
+Story = "One day " + person_name + " went to the " + event + " . " + person_name + "was enjoying it so much that the" + name_of_a_food + "spilled all over the floor from their" + adjective + " moves. The " + job_title + " was not very " + emotion + " because he had to clean up the mess. " + person_name + " ended up getting kicked out of the " + event + ". As the main guest, " + celebrity + " arrived, everyone started " + verb_with_ing + " at their " + body_part + ". " + person_name + " said that " + celebrity + " gave " + number + " dollars to them. I feel " + emotion + " for " + person_name + "."
+
+print(Story)
+
+
+
