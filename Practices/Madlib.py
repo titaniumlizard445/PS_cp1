ccb0902284053634ebfba1af7016c5480a170eba
titaniumlizard445 <parker.swenson@ucas-edu.net>
2025-09-03 16:50:06 -0600

    puuuuuuuuush

--- Practices/Madlib.py
+++ Practices/Madlib.py
@@ -13,7 +13,7 @@ adjective = input("name an adjective: ")
 
 
 
-Story = "One day " + person_name + " went to the " + event + " . " + person_name + "was enjoying it so much that the" + name_of_a_food + "spilled all over the floor from their" + adjective + " moves. The " + job_title + " was not very " + emotion + " because he had to clean up the mess. " + person_name + " ended up getting kicked out of the " + event + ". As the main guest, " + celebrity + " arrived, everyone started " + verb_with_ing + " at their " + body_part + ". " + person_name + " said that " + celebrity + " gave " + number + " dollars to them. I feel " + emotion + " for " + person_name + "."
+Story = "One day " + person_name + " went to the " + event + " . " + person_name + " was enjoying it so much that the " + name_of_a_food + " spilled all over the floor from their " + adjective + " dance moves. The " + job_title + " was not very " + emotion + " because he had to clean up the mess. " + person_name + " ended up getting kicked out of the " + event + ". As the main guest, " + celebrity + " arrived, everyone started " + verb_with_ing + " at their " + body_part + ". " + person_name + " said that " + celebrity + " gave " + number + " dollars to them. I feel " + emotion + " for " + person_name + "."
 
 print(Story)
 
