2de590172af793e1d5f38d4e02c128ffb8e0c19f
titaniumlizard445 <parker.swenson@ucas-edu.net>
2025-09-09 09:54:13 -0600

    Almost finished

--- Practices/Basic_Calculator.py
+++ Practices/Basic_Calculator.py
@@ -3,12 +3,34 @@
 running = True
 
 while running:
-    number1 = int(input("Enter your first number"))
-    number2 = int(input("Enter your second number"))
-    operation = input("Which operator would you like to use?(Hint: write the first two letters of the operations name): ")
+    number1 = int(input("Enter your first number").strip())
+    number2 = int(input("Enter your second number").strip())
+    operation = input("Which operator would you like to use?(Hint: write the first three letters of the operations name): ").strip()
 
-    if operation == "ad":
+    if operation == "add":
         number3 = number1 + number2
+        print(f"{number1} + {number2} = {number3:2f}")
+    elif operation == "sub":
+        number3 = number1 - number2
+        print(f"{number1} - {number2} = {number3:2f}")
+    elif operation == "mul":
+        number3 = number1 * number2
+        print(f"{number1} x {number2} = {number3:2f}")
+    elif operation == "div":
+        number3 = number1 / number2
+        print(f"{number1} / {number2} = {number3:2f}")
+    elif operation == "mod":
+        number3 = number1 % number2
+        print(f"{number1} % {number2} = {number3:2f}")
+    elif operation == "exp":
+        number3 = number1 ** number2
+        print(f"{number1}^{number2} = {number3:2f}")
+    elif operation == "flo":
+        number3 = number1 // number2
+        print(f"{number1} // {number2} = {number3:2f}")
+    elif operation == "ad":
+        number3 = number1 + number2
+        print(f"{number1} + {number2} = {number3:2f}")
     
-
-    print(number3)
+    stop = input("Do you want to terminate the program")
+    
