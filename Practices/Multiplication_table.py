11db00200fd97d086580f709021105f2e131e2f2
titaniumlizard445 <parker.swenson@ucas-edu.net>
2025-10-02 09:54:55 -0600

    push

--- Practices/Multiplication_table.py
+++ Practices/Multiplication_table.py
@@ -1,8 +1,12 @@
 #PS 1st Multiplication table
 
-row = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-
-for i in row:
-    print(row)
-    for a in row:
-        row[a] *= i
+row = [1,2,3,4,5,6,7,8,9,10,11,12]
+new_row = []
+#Prints the calculated rows 
+for a in row:
+    for x in range(len(row)):
+        new_element = row[x] * a
+        new_row.append(new_element)
+        #multiplies each row
+    new_row.append(new_element)
+print(list[new_row])
