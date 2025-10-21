3be0cd469a9a17f2f232f7caa5f659a2b8964486
titaniumlizard445 <parker.swenson@ucas-edu.net>
2025-10-21 09:06:57 -0600

    Message

---
+++ Notes/FUNctions.py
@@ -0,0 +1,37 @@
+#PS 1st FUNctions
+
+#set variables first then functions
+
+
+def coolFuntionName():
+    print("Parker wins")
+
+coolFuntionName()
+
+def coolNewFuntion(name,fav_num):
+    print(f"Your name is {name} And your favorite number is {fav_num}")
+
+coolNewFuntion("Robert",345)
+
+def add(x,y):
+    return x+y
+
+total = add(5,5)
+print(total)
+print(f"10 + 72 = {add(10,72)}")
+
+x = 0
+
+while x < add(3,9):
+    print("duck")
+    x+=1
+
+print("goose")
+
+def initials(Name):
+    names = Name.split(" ")
+    initials = ""
+    for Name in names:
+        initials += Name[0]
+    
+    return initials
