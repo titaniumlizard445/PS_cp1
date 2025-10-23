cc8470f81ed3e861d545a087312e9e9f1e058fdc
titaniumlizard445 <parker.swenson@ucas-edu.net>
2025-10-22 20:11:50 -0600

    Update Caesar_Cipher.py

--- Practices/Caesar_Cipher.py
+++ Practices/Caesar_Cipher.py
@@ -8,18 +8,34 @@ def Encrypt(Message, Cipher_Number):
     Letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     new_message = ""
     for x in Message:
+      if x == " ":
+        new_message = new_message + x
+      else:
         Position = Letters.index(x)
         Position += Cipher_Number
-        new_message = new_message + Letters(Position)
+        Position -= 26
+        new_message = new_message + Letters[Position]
         new_message = new_message.capitalize()
     return new_message
 
 
 
 #function for Decrypting
-def Decrypt(Message, Ciper_Number):
-    Letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+def Decrypt(Message, Cipher_Number):
     Message = Message.lower()
+    Letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    new_message = ""
+    for x in Message:
+      if x == " ":
+        new_message = new_message + x
+      else:
+        Position = Letters.index(x)
+        Position -= Cipher_Number
+        if Position > 26:
+          Position += 26
+        new_message = new_message + Letters[Position]
+        new_message = new_message.capitalize()
+    return new_message
 
 
 #asks user if they want to decrypt or encrypt
@@ -31,9 +47,9 @@ Message = input("Write a message to be encrypted or decrypted \n Rules \n No Num
 Cipher = int(input("What is the ciper number you would like to use? \n Put an integer here: ").strip())
 if Mode == 1:
     print("Encrypter chosen")
-    Encrypt(Message, Cipher)
+    print(Encrypt(Message, Cipher))
 elif Mode == 2:
     print("Decrypter chosen ")
-    Decrypt(Message, Cipher)
+    print(Decrypt(Message, Cipher))
 else:
     print("Invalid Input")
