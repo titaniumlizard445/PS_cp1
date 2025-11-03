06345497c68ccdf68438586c8656b21413ae0a3a
titaniumlizard445 <parker.swenson@ucas-edu.net>
2025-11-03 09:53:40 -0700

    building mazes is hard D:

--- Practices/Maze_of_doom.py
+++ Practices/Maze_of_doom.py
@@ -5,23 +5,44 @@ import random
 
 #types of walls the code can make
 size_of_squares = 25
-walls = ["_","| "," |","-"]
-bottom_and_top_row_ = ["_","_","_","_","_","_","_","_","_","_","_","_"," ","_","_","_","_","_","_","_","_","_","_","_","_","_"]
-row = []
-maze = [row,row,row,row,row,row,row,row,row,row,row,row,row,row,row,row,row,row,row,row,row,row,row,row,row,row]
+walls = ["_","| "," |","-"," "]
+#all the rows in the grid
+grid_rows =  [[],[],[],[],[],[],[],[]]
+#columns
+grid_columns = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+
+for row in grid_rows:
+    for space in row:
+        grid_rows[row[space]] = random.choice(walls)
+
+print(grid_rows)
 #functions for drawing each type of wall
+
 
 def left_side_wall():
     print("bob")
-    
+
+#Maze solvable function
+def solvability():
+    print("bob")
+
+
 
 #create turtle
 Bob_the_builder = turtle.Turtle()
 
-#loop to make the whole maze
-for x in maze:
-    if x == 0:
-        print("bob")
+#draw outside boundary
 
-#draw predetermined top row
-#draw the row
+Bob_the_builder.penup()
+Bob_the_builder.goto(-250,250)
+Bob_the_builder.pendown()
+Bob_the_builder.forward(250)
+Bob_the_builder.penup()
+Bob_the_builder.forward(25)
+Bob_the_builder.pendown()
+Bob_the_builder.forward(250)
+Bob_the_builder.right(90)
+
+
+
+turtle.done()
