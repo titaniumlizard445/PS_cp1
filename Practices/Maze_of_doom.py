f1034241cfcbc6dec79811b49015ade3a04a35a4
titaniumlizard445 <parker.swenson@ucas-edu.net>
2025-11-04 09:54:25 -0700

    WE DID IT YIPEE

--- Practices/Maze_of_doom.py
+++ Practices/Maze_of_doom.py
@@ -5,22 +5,47 @@ import random
 
 #types of walls the code can make
 size_of_squares = 25
-walls = ["_","| "," |","-"," "]
+F = False
+T = True
+Bools = [T,F]
+ex_col = [F,F,F,F,F,F,F,F]
+ex_row = [F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F]
 #all the rows in the grid
-grid_rows =  [[],[],[],[],[],[],[],[]]
+grid_rows =  []
 #columns
-grid_columns = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+grid_columns = []
 
-for row in grid_rows:
-    for space in row:
-        grid_rows[row[space]] = random.choice(walls)
+#updating a column
+for x in range(0,19):
+    for y in range(0,8):
+        #change each part of a column
+        ex_col[y] = random.choice(Bools)
+    #add that column to the maze
+    grid_columns.append(ex_col)
+    ex_col = [F,F,F,F,F,F,F,F]
 
+print(grid_columns)
+
+for x in range(0,10):
+    for y in range(0,19):
+        #change each part of a row
+        ex_row[y] = random.choice(Bools)
+    #add that new randomized row to the grid
+    grid_rows.append(ex_row)
+    ex_row = [F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F]
 print(grid_rows)
-#functions for drawing each type of wall
 
 
-def left_side_wall():
+
+
+#functions for drawing each wall
+
+
+def vertical_wall():
     print("bob")
+
+def horizontal_wall():
+    print("the ram")
 
 #Maze solvable function
 def solvability():
@@ -42,6 +67,15 @@ Bob_the_builder.forward(25)
 Bob_the_builder.pendown()
 Bob_the_builder.forward(250)
 Bob_the_builder.right(90)
+Bob_the_builder.forward(250)
+Bob_the_builder.right(90)
+Bob_the_builder.forward(250)
+Bob_the_builder.penup()
+Bob_the_builder.forward(25)
+Bob_the_builder.pendown()
+Bob_the_builder.forward(250)
+Bob_the_builder.right(90)
+Bob_the_builder.forward(250)
 
 
 
