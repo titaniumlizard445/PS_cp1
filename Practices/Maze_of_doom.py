29e03412f8836104bdf872b9ecd44ce91f786714
titaniumlizard445 <parker.swenson@ucas-edu.net>
2025-11-05 09:54:20 -0700

    Uh i did something I think

--- Practices/Maze_of_doom.py
+++ Practices/Maze_of_doom.py
@@ -37,21 +37,45 @@ print(grid_rows)
 
 
 
-
-#functions for drawing each wall
-
-
-def vertical_wall():
-    print("bob")
-
-def horizontal_wall():
-    print("the ram")
-
 #Maze solvable function
-def solvability():
-    print("bob")
+def solvability(grid_row,grid_col):
+    #coords are realtive to the walls not the turtle
+    #starting position is going to be
+    to_be_checked_coords = [(11,0)]
+    #vars for coords
+    visited = []
+    maze_size = len(grid_row) - 1
+    while to_be_checked_coords:
+        #get coordinates for checking
+        x , y = to_be_checked_coords.pop()
+
+        if x == maze_size-1 and y == maze_size -1:
+            return True
+        
+        if (x , y) in visited: 
+            continue
+
+        #adds the new coordinates to the checked portion
+
+        visited.append((x,y))
 
 
+    #checks if there is a wall north of it
+        if y > 0 and grid_row[y][x] == False:
+            to_be_checked_coords.append((x,y-1))
+    #checks if there is a wall west of it
+        if x > 0 and grid_col[y][x] == False:
+            to_be_checked_coords.append((x-1,y))
+
+    #checks if there is a wall east of it
+        if x < maze_size - 1 and grid_col[y][x+1] == False:
+            to_be_checked_coords.append((x+1,y))
+
+    #checks if there is a wall south of it
+        if y < maze_size -1 and grid_row[y+1][x] == False:
+            to_be_checked_coords.append((x,y+1))
+
+solvability(grid_rows,grid_columns)
 
 #create turtle
 Bob_the_builder = turtle.Turtle()
@@ -76,7 +100,16 @@ Bob_the_builder.pendown()
 Bob_the_builder.forward(250)
 Bob_the_builder.right(90)
 Bob_the_builder.forward(250)
-
+Bob_the_builder.right(90)
+#draw the walls inside
+for w in grid_rows:
+    Bob_the_builder.goto(-250,225)
+    for a in w:
+        if w == F:
+            Bob_the_builder.penup()
+        elif w == T:
+            Bob_the_builder.pendown()
+        Bob_the_builder.forward(1.25)
 
 
 turtle.done()
