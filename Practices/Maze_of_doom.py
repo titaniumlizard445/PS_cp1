417c0a6d60a71138316db54ad9c3cd2f27198aeb
titaniumlizard445 <parker.swenson@ucas-edu.net>
2025-11-06 09:53:36 -0700

    THERE IS A MAZE! :D

--- Practices/Maze_of_doom.py
+++ Practices/Maze_of_doom.py
@@ -8,31 +8,31 @@ size_of_squares = 25
 F = False
 T = True
 Bools = [T,F]
-ex_col = [F,F,F,F,F,F,F,F]
-ex_row = [F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F]
+ex_col = [F,F,F,F,F,F,F,F,F,F]
+ex_row = [F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F]
 #all the rows in the grid
 grid_rows =  []
 #columns
 grid_columns = []
 
 #updating a column
-for x in range(0,19):
-    for y in range(0,8):
+for x in range(0,21):
+    for y in range(0,10):
         #change each part of a column
         ex_col[y] = random.choice(Bools)
     #add that column to the maze
     grid_columns.append(ex_col)
-    ex_col = [F,F,F,F,F,F,F,F]
+    ex_col = [F,F,F,F,F,F,F,F,F,F]
 
 print(grid_columns)
 
-for x in range(0,10):
-    for y in range(0,19):
+for x in range(0,9):
+    for y in range(0,20):
         #change each part of a row
         ex_row[y] = random.choice(Bools)
     #add that new randomized row to the grid
     grid_rows.append(ex_row)
-    ex_row = [F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F]
+    ex_row = [F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F,F]
 print(grid_rows)
 
 
@@ -75,41 +75,67 @@ def solvability(grid_row,grid_col):
         if y < maze_size -1 and grid_row[y+1][x] == False:
             to_be_checked_coords.append((x,y+1))
 
-solvability(grid_rows,grid_columns)
+#solvability(grid_rows,grid_columns)
 
 #create turtle
 Bob_the_builder = turtle.Turtle()
 
 #draw outside boundary
+def Outside_boundary():
+    Bob_the_builder.speed(0)
+    Bob_the_builder.penup()
+    Bob_the_builder.goto(-250,250)
+    Bob_the_builder.pendown()
+    Bob_the_builder.forward(250)
+    Bob_the_builder.penup()
+    Bob_the_builder.forward(25)
+    Bob_the_builder.pendown()
+    Bob_the_builder.forward(250)
+    Bob_the_builder.right(90)
+    Bob_the_builder.forward(250)
+    Bob_the_builder.right(90)
+    Bob_the_builder.forward(250)
+    Bob_the_builder.penup()
+    Bob_the_builder.forward(25)
+    Bob_the_builder.pendown()
+    Bob_the_builder.forward(250)
+    Bob_the_builder.right(90)
+    Bob_the_builder.forward(250)
+    Bob_the_builder.right(90)
 
-Bob_the_builder.penup()
-Bob_the_builder.goto(-250,250)
-Bob_the_builder.pendown()
-Bob_the_builder.forward(250)
-Bob_the_builder.penup()
-Bob_the_builder.forward(25)
-Bob_the_builder.pendown()
-Bob_the_builder.forward(250)
-Bob_the_builder.right(90)
-Bob_the_builder.forward(250)
-Bob_the_builder.right(90)
-Bob_the_builder.forward(250)
-Bob_the_builder.penup()
-Bob_the_builder.forward(25)
-Bob_the_builder.pendown()
-Bob_the_builder.forward(250)
-Bob_the_builder.right(90)
-Bob_the_builder.forward(250)
-Bob_the_builder.right(90)
+Outside_boundary()
 #draw the walls inside
+
+#horizontal walls
+Bob_the_builder.goto(-250,225)
+iterator = 0
 for w in grid_rows:
-    Bob_the_builder.goto(-250,225)
+    iterator += 1
+    Bob_the_builder.goto(-250,iterator*25)
+    #draws a wall when there is true else it has the pen up and moves forward
     for a in w:
-        if w == F:
+        if a == False:
             Bob_the_builder.penup()
-        elif w == T:
+        elif a == True:
             Bob_the_builder.pendown()
-        Bob_the_builder.forward(1.25)
+        Bob_the_builder.forward(25)
+        Bob_the_builder.penup()
+
+
+Bob_the_builder.goto(250,250)
+iterator = 0
+Bob_the_builder.right(90)
+for w in grid_columns:
+    iterator += 1
+    Bob_the_builder.goto(275-(25*iterator),250)
+    #draws a wall when there is true else it has the pen up and moves forward
+    for a in w:
+        if a == False:
+            Bob_the_builder.penup()
+        elif a == True:
+            Bob_the_builder.pendown()
+        Bob_the_builder.forward(25)
+        Bob_the_builder.penup()
 
 
 turtle.done()
