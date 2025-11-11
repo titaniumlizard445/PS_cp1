15025b3fea81989114516a685a4f024fe1e6d6c5
titaniumlizard445 <parker.swenson@ucas-edu.net>
2025-11-11 09:54:07 -0700

    Commit and Push

--- Practices/Order_Up.py
+++ Practices/Order_Up.py
@@ -1,8 +1,86 @@
 #PS 1st Order Up (make a menu and give total cost of user selection)'
 
+Side_dishes_ordered = 0
+order = []
+
+#options for hamburgers and how much they are
 Bamhurgers = {
-    "CheeseBurger":7,
-    "Double Pattie":15,
-    "Hamburger":5,
-    "Western Bacon":12
+    "CheeseBurger":7.00,
+    "Double Pattie":15.00,
+    "Hamburger":3.00,
+    "Western Bacon":12.00,
+    "Classic Bamhurger":5.00,
+    "Burrito Bamhurger":10.00,
+    "Breakfast Bamhurger":25.00
 }
+ 
+#options for drinks and how much they are
+Dinks = {
+    "Mylkshakes":{
+        "Chocolate":10.00,
+        "Vanilla":100.00,
+        "Mint Chocolate":8.00
+    },
+    "Super duper delicious Dihydrogen Monoxide from a fresh spring in the Switzerland Alps":100.00,
+    "approximately 3.14159265358979323846 oz of tap water":3.14,
+    "Orange Juice":4.00,
+    "Lemonade":2.50,
+    "Dehydrated water (to make water just add water)":1000.00
+}
+#options for sides and how much they are
+sides = {
+    "French Fries": 3.00,
+    "In-n-Out Fries":10.00,
+    "Onion Rings":5.00,
+    "Mac & Cheese":9.00,
+    "salad":2.50
+}
+#whole menu
+reasonably_price_menu = {
+    "Hamburgers":Bamhurgers,
+    "Drinks":Dinks,
+    "Sides":sides
+}
+
+print("Hello and welcome to the Reasonably priced Restaurant located directly above I-15\n\nThe Freedom Diner\n\n")
+
+Ordering = True
+while Ordering:
+    #type of food they are ordering
+    selection = input("What type of food would you like to order? \n 1) Bamhurgers \n 2) Drinks \n 3) Sides \n Please input the number here: ").strip()
+
+    #ordering Hamburgers
+    if selection == "1":
+        print("Please select one of the following to order with the prices below each respectively ")
+
+        #Display Options
+
+        print(Bamhurgers.keys())
+        print(Bamhurgers.values())
+
+        #ask user what they want
+
+    #ordering Drinks
+    if selection == "2":
+        print("Please select one of the following to order with the prices below each respectively")
+
+        #Display Options
+
+        print(Dinks.keys())
+        print(Dinks.values())
+
+        #ask user what they want
+
+    #ordering two sides
+    if selection == "3":
+        print("Please select one of the following to order with the prices below each respectively")
+
+        #Display Options
+
+        print(sides.keys())
+        print(sides.values())
+        
+        #ask user what they want
+
+    #finish order
+        #check to make sure that the user has ordered two side dishes
