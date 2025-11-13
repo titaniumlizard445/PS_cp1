b53aa9ee8ab185784f910c32d41a36d2a48f2149
titaniumlizard445 <parker.swenson@ucas-edu.net>
2025-11-13 09:53:11 -0700

    Yipee it works properly

--- Practices/Order_Up.py
+++ Practices/Order_Up.py
@@ -48,8 +48,14 @@ reasonably_price_menu = {
 def IndividualMenu(Food_type):
     options = Food_type.keys()
     prices = Food_type.values()
-    print({options})
-    print({prices})
+    print(options)
+    print(prices)
+
+def DisplayOrder(order):
+    print(f"Your Order is: \n")
+    for x in order:
+        print(f"{x}")
+    print(f"\nThe cost of your order is: ${Cost}\nThank You for Your Order\nPlease Proceed to the payment window")
 
 print("Hello and welcome to the Reasonably priced Restaurant located directly above I-15\n\nThe Freedom Diner\n\n")
 
@@ -67,6 +73,7 @@ while Ordering:
         IndividualMenu(Bamhurgers)
         #ask user what they want
         User_choice = input("What type of Bamhurger do you want? \n write here: ").strip()
+        #checks if the burger is orderable
         if User_choice in Bamhurgers:
             order.append(User_choice)
             Cost += Bamhurgers[User_choice]
@@ -79,8 +86,20 @@ while Ordering:
         #Display Options
         IndividualMenu(Dinks)
         #ask user what they want
-        User_choice = input("What type of Bamhurger do you want? \n write here: ").strip()
-        if User_choice in Dinks:
+        User_choice = input("What type of Drink do you want? \n write here: ").strip()
+        #checks if it is a milkshake for special circumstances
+        if User_choice == "Mylkshakes":
+            #displays shake options
+            print(f"Your options for a shake are {Dinks["Mylkshakes"]}")
+            #asks user for shake that they want
+            Ordered_Milkshake = input("which Mylkshake would you like to order?\nWrite here:  ").strip()
+            if Ordered_Milkshake in Dinks["Mylkshakes"]:
+                order.append(Dinks["Mylkshakes"][Ordered_Milkshake])
+                Cost += Dinks["Mylkshakes"][Ordered_Milkshake]
+            else:
+                print("Please Try again")
+        #checks if normal drinks are orderable
+        elif User_choice in Dinks:
             order.append(User_choice)
             Cost += Dinks[User_choice]
         else:
@@ -93,25 +112,27 @@ while Ordering:
         IndividualMenu(sides)
         #ask user what they want
 
-        User_choice = input("What type of Bamhurger do you want? \n write here: ").strip()
+        User_choice = input("What type of Side do you want? \n write here: ").strip()
+        #checks if the Side is orderable
         if User_choice in sides:
             order.append(User_choice)
             Cost += sides[User_choice]
+            Side_dishes_ordered += 1
         else:
             print("Invalid Input Please Try Again (Make sure to write it as it is in the Menu)")
 
     #finish order
-    done = input("Are you done ordering?").strip().lower()
+    done = input("Are you done ordering? ").strip()
+    done = done.lower()
+    
     if done == "yes" or  done == "y":
         #check to make sure that the user has ordered two side dishes
         if Side_dishes_ordered >= 2:
             Ordering = False
         else:
+            print(f"You Have Ordered {Side_dishes_ordered} Side Dishes")
             print("Please order two or more side dishes")
     else:
         print("\n Continue Ordering \n")
 
-print(f"Your Order is: ")
-for x in order:
-    print(f"{x}\n")
-print(f"The cost of your order is: {Cost}")
+DisplayOrder(order)
