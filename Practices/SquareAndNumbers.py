f59c1231a8d795fdd3022c1de551ff9a87494e55
titaniumlizard445 <parker.swenson@ucas-edu.net>
2025-11-19 09:50:56 -0700

    made 3 lines of code today!

--- Practices/SquareAndNumbers.py
+++ Practices/SquareAndNumbers.py
@@ -4,5 +4,4 @@ data=[1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,
 #lambda function for sqring data
 squarer=list(map(lambda num:num**2,data))
 #display data for each on individually
-for num in squarer:
-    print(f"Original number:{data[squarer.index(num)]} Squared Number: {num}")
+for num in squarer:print(f"Original number:{data[squarer.index(num)]} Squared Number: {num}")
