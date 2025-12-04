058294d96fd54e7f34ca2fefa35767dab7d36bc9
titaniumlizard445 <parker.swenson@ucas-edu.net>
2025-12-04 09:53:54 -0700

    AI is good for story time

---
+++ THE FINAL/UltimateTextBasedAdventureGame.py
@@ -0,0 +1,95 @@
+#PS 1st The Adventure of John Jonathan Johnson
+
+#Libraries
+import random
+import time
+
+#Var
+
+player_stats = {}
+player_inventory = []
+
+enemies = {}
+
+village_shops = {}
+freed_villages = []
+
+resetdictionary= {}
+
+name = ""
+
+start_time = 0
+end_time = 0
+
+total_defeated = 0
+
+#Village Name : Codes: reward/puzzle
+secret_codes = {}
+
+#score board contains names, points and times
+scoreboard = {}
+
+enemies_defeated = 0
+#funct
+
+def PlayerTurn():
+    print("Insert stuff here")
+
+def MonsterTurn():
+    print("Insert stuff here")
+
+def BossTurn():
+    print("Insert stuff here")
+
+def ItemDropSystem():
+    print("Insert stuff here")
+
+def OpenInventory():
+    print("Insert stuff here")
+
+def CombatSystem():
+    print("Insert stuff here")
+
+def Chestsystem():
+    print("Insert stuff here")
+
+def Rest():
+    print("John slept and remained unproductive the whole night")
+
+def FreeVillage():
+    print("Put combat function with for x3 and other stuff")
+
+#take in dictionary for items and village be accessed
+def UseShop():
+    print("Insert stuff here")
+
+def Credits():
+    print("Insert cool stuff here")
+
+def JohnsHouse():
+    print("Insert stuff here")
+
+#(parameters) take in name and dictionary with info and villages defeated
+def DefaultVillage():
+    print("Insert stuff here")
+
+def BastilliaFortuica():
+    print("Insert stuff here")
+
+def TutorialIntroduction():
+    print("Insert stuff here")
+
+#gameloop
+while True:
+    print("Welcome to possibly the greatest text based adventure game you will see today \nIf you care about your score: \nthere are three stats that will be recordeed on the scoreboard \n1)Your Time \n2)The Number of Enemies you Defeated \n3)Your Intelligence stat \n")
+    
+    #game
+    while True:
+        print("")
+
+
+
+#scoreboard
+
+
+
